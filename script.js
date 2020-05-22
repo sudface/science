d3cefca296f91c80c7131c0f041c1f4f3941bb35
Sudarshan <31204774+sudface@users.noreply.github.com>
2020-05-22 14:38:51 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/science/sw.js', {scope: '/science/'}).then(function(registration) {
+    navigator.serviceWorker.register('./sw.js', {scope: '/science/'}).then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
